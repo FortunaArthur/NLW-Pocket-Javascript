5d63450298ff2fee646ec35db91f240dd5f51762
FortunaArthur <arthur.trabalho2646@gmail.com>
2024-09-10 20:51:52 -0300

    aula1

---
+++ aula1/index.js
@@ -0,0 +1,34 @@
+// // salve men
+// console.log("salve")
+
+//  // lista/ variavel
+// let lista = ["jooj", "bahia"]
+// console.log(lista[1] + ", " + lista[0])
+
+// // array/ obejto / metodo
+// let meta ={
+//     value: "ler",
+//     checked: false,
+//     log: (info) => {console.log(info)}}
+
+// meta.log(meta.value)
+
+// meta.value = "MAKAKO"
+// meta.log(meta.value)
+
+let meta = {
+    valor: "jooj",
+    chekado: true,
+}
+
+let metas = [
+    meta,
+    {
+        valor: "corre dos 20",
+        chekado: false,
+    }
+]
+
+console.log(metas[0].valor)
+
+console.log(metas[1].chekado)
